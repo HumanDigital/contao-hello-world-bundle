62458552e08011bc3c0cf92ab31b4686b39ada17
HumanDigital <github@humandigital.ch>
2018-11-21 15:54:05 +0000

    Create hello-world frontend module

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Frontend modules
+$GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'Humandigital\ContaoHelloWorldBundle\Module\HelloWorldModule';
+
+
+/**
+ * Front end modules
+ *	Home: die nächsten x Termine
+ *  Agenda: alle Termine nach Datum sortiert und pro Monat
+ *	Angebot: Alle Führungen auch auf Anfrage
+ */
+/*
+$GLOBALS['FE_MOD']['miscellaneous'] = array 
+( 
+        'helloWorld' 		=> 'HelloWorldModule' ,
+);
+*/
